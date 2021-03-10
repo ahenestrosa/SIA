79a2682d1c85979d98ce6f77f9e0447f9162fd27
francisco choi <fchoi@itba.edu.ar>
2021-03-09 22:46:36 -0300

    Bfs finished

--- TP1/BFS/Bfs.py
+++ TP1/BFS/Bfs.py
@@ -8,41 +8,41 @@ class Bfs:
     movements = [Constants.UP, Constants.DOWN,Constants.LEFT, Constants.RIGHT]
     def __init__(self, rootBoard):
         self.explored = []
-        self.root = Node(rootBoard, self.depth)
+        node = Node(rootBoard, self.depth)
+        self.root = node
         self.queue = deque()
-        self.queue.append(rootBoard)
+        self.queue.append(node)
 
     def start(self):
-        while len(self.queue) > 0:
+        node = {}
+        while len(self.queue) > 0 and (node == {} or not node.sokoban.isGameFinished()):
             node = self.queue.popleft()
-            node.board.printBoard()
             goingUpNode = Node(Sokoban.from_game(node.sokoban), node.depth + 1, node)
             goingDownNode = Node(Sokoban.from_game(node.sokoban), node.depth + 1, node)
             goingLeftNode = Node(Sokoban.from_game(node.sokoban), node.depth + 1, node)
             goingRightNode = Node(Sokoban.from_game(node.sokoban), node.depth + 1, node)
 
-            if node.sokoban.isGameFinished() == False:
-                if(goingUpNode.sokoban.move(Constants.UP) == Constants.VALID_MOVE and self._not_explored_board(goingUpNode) == True):
+            if not node.sokoban.isDeadEnd():
+                if(goingUpNode.sokoban.move(Constants.UP) == Constants.VALID_MOVE and self._not_explored_board(goingUpNode)):
                     node.appendChild(goingUpNode)
                     self.queue.append(goingUpNode)
-                if(goingDownNode.sokoban.move(Constants.DOWN) == Constants.VALID_MOVE and self._not_explored_board(goingDownNode) == True):
+                if(goingDownNode.sokoban.move(Constants.DOWN) == Constants.VALID_MOVE and self._not_explored_board(goingDownNode)):
                     node.appendChild(goingDownNode)
                     self.queue.append(goingDownNode)
-                if(goingLeftNode.sokoban.move(Constants.LEFT) == Constants.VALID_MOVE and self._not_explored_board(goingLeftNode) == True):
+                if(goingLeftNode.sokoban.move(Constants.LEFT) == Constants.VALID_MOVE and  self._not_explored_board(goingLeftNode)):
                     node.appendChild(goingLeftNode)
                     self.queue.append(goingLeftNode)
-                if(goingRightNode.sokoban.move(Constants.RIGHT) == Constants.VALID_MOVE and self._not_explored_board(goingRightNode) == True):
+                if(goingRightNode.sokoban.move(Constants.RIGHT) == Constants.VALID_MOVE and self._not_explored_board(goingRightNode)):
                     node.appendChild(goingRightNode)
                     self.queue.append(goingRightNode)
 
                 self.explored.append(node) #already explored
-            else:
-                #game finishes
-                print('game finished');
+        node.sokoban.printBoard();
+        self.root.sokoban.printBoard()
 
     def _not_explored_board(self, node):
         for exp in self.explored:
-            if exp.redundant_equal(node):
+            if exp.redundant_equal(node) and node.depth > exp.depth:
                 return False
         return True
 
