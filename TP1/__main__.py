950bf9464a6da7230d6c7ec94d60090cee0215a8
Nicolas de la Torre <ndelatorre@itba.edu.ar>
2021-03-12 08:44:11 -0300

    add configuration

--- TP1/__main__.py
+++ TP1/__main__.py
@@ -4,44 +4,60 @@ from BFS.Bfs import Bfs
 from DFS.Dfs import Dfs
 from collections import deque
 import time
-
-# objective = [(0,0), (2,2)]
-# dimensions = (5,5)
-# boxes = [(0,1), (1, 2)]
-# walls = [(3,3), (4,4)]
-# player = (0, 2)
-# sokoban = Sokoban(walls, objective, dimensions, player, boxes)
+import json
 
 
-objective = [(2,1),(3,1)]
-dimensions = (6,6)
-boxes = [(2,3), (3,3)]
-walls = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),
-          (1,0),(2,0),(3,0),(4,0),(5,0),
-          (5,1),(5,2),(5,3),(5,4),
-          (1,5),(2,5),(3,5), (4,5)]
-player = (1, 4)
-sokoban = Sokoban(walls, objective, dimensions, player, boxes)
+def main():
+    
+    # objective = [(2,5),(6,5)]
+    # dimensions = (9,9)
+    # boxes = [(5,5), (3,5)]
+    # walls = [(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(0,6),(0,7),(0,8),
+    #         (1,0),(2,0),(3,0),(4,0),(5,0),(6,0),(7,0),(8,0),
+    #         (8,1),(8,2),(8,3),(8,4),(8,5),(8,6),(8,7),(8,8),
+    #         (1,8),(2,8),(3,8), (4,8),(5,8),(6,8),(7,8)]
+    # player = (4, 5)
+    # sokoban = Sokoban(walls, objective, dimensions, player, boxes)
+
+    objective = [(0,0), (2,2)]
+    dimensions = (5,5)
+    boxes = [(0,1), (1, 2)]
+    walls = [(3,3), (4,4)]
+    player = (0, 2)
+    sokoban = Sokoban(walls, objective, dimensions, player, boxes)
+
+    with open('TP1/config.json') as config:
+        data = json.load(config)
+
+    algorithm = data['algorithm']
+    level_map = data['level_map']
+    iddfs_max_depth = data["iddfs_max_depth"]
+    print("Algorithm is:", algorithm)
+    print("Max depth for IDDFS is:", iddfs_max_depth)
+    print()
 
 
-# board.printBoard()
-# board.move(Constants.UP)
-# board.printBoard()
-# board.move(Constants.DOWN)
-# board.move(Constants.RIGHT)
-# board.printBoard()
-# print(board.isGameFinished())
-# newBoard = Board.from_game(board)
-# newBoard.move(Constants.RIGHT)
-# newBoard.printBoard()
-#
-# board.printBoard()
-# print(newBoard.redundant_equal(board))
-# print(sokoban.move(Constants.UP))
-start = time.time()
-algorithm = Bfs(sokoban)
-algorithm.start()
-end = time.time()
+    if algorithm == "bfs":
+        start = time.time()
+        aux = Bfs(sokoban)
+        sol = aux.start
+        print(time.time() - start)
 
-print(end - start)
+    elif algorithm == "dfs":
+        start = time.time()
+        aux = Dfs(sokoban)
+        sol = aux.start()
+        print(time.time() - start)
+
+    # elif algorithm == "iddfs":
+    #     sol = iddfs(game, initial_node, iddfs_depth_limit)
+    
+    else:
+        print("Invalid algorithm.")
+        exit()
+
+
+if __name__ == "__main__":
+    main()
+
 
