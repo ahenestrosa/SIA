d50483f897c040886e5dcd8d7c8391975e792374
ahenestrosa <ahenestrosa@itba.edu.ar>
2021-03-26 17:29:29 -0300

    Genes definition

---
+++ TP2/Crossing/Crossing.py
@@ -0,0 +1,28 @@
+from Utilities import Constants
+from random import randrange, uniform
+
+class Crossing:
+
+    @staticmethod
+    def onePointCrossing(parent1, parent2):
+        gN = randrange(len(Constants.GENES_LIST)))
+        gen = Constants.GENES_LIST[gN]
+
+        if gn == Constants.GEN_CLASS:
+            
+        elif gn == Constants.GEN_HEIGHT:
+
+        else:
+
+
+    @staticmethod
+    def twoPointCrossing(parent1, parent2):
+        return None
+
+    @staticmethod
+    def anularCrossing(parent1, parent2):
+        return None
+
+    @staticmethod
+    def unifromCrossing(parent1, parent2):
+        return None
