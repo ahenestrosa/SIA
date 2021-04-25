c0538adccd4068e624b4af78463e3a1890c33889
Nicolas de la Torre <ndelatorre@itba.edu.ar>
2021-04-25 11:42:40 -0300

    simple perceptron non linear done

--- TP3/SimpleNonLinearPerceptron.py
+++ TP3/SimpleNonLinearPerceptron.py
@@ -1,21 +1,20 @@
 import sys
 import numpy as np
+import math
 
 
 class SimpleNonLinearPerceptron:
 
-    def __init__(self, inputSize, learningRate, g, gPrime):
+    def __init__(self, inputSize, learningRate):
         self.weightSize = inputSize + 1
         self.learningRate = learningRate
         self.weights = np.random.random(self.weightSize)
-        # self.weights = [0] * (self.weightSize)
-        self.g = g
-        self.gPrime = gPrime
+
 
 
     def trainPerceptron(self, trainingSet, epsilon, epoch):
         minW = self.weights
-        X = [ [[-1, t[0] , t[1], t[2]] , t[3]] for t in trainingSet ]
+        X = [ [[1, t[0][0] , t[0][1], t[0][2]] , t[1]] for t in trainingSet ]
         # print(X)
         error = sys.maxsize # https://stackoverflow.com/questions/7604966/maximum-and-minimum-values-for-ints
         minError = error
@@ -25,10 +24,9 @@ class SimpleNonLinearPerceptron:
             i_x = np.random.randint(0, len(trainingSet))
 
             excitation = np.inner(self.weights, X[i_x][0])
-            gPrimeValue = self.gPrime(excitation)
-            deltaW = (X[i_x][1] - self.g(excitation)) * gPrimeValue * self.learningRate * np.array(X[i_x][0])
+            gPrimeValue = (1 - math.tanh(excitation)**2)
+            deltaW = (X[i_x][1] - self.activation(excitation)) * gPrimeValue * self.learningRate * np.array(X[i_x][0])
             self.weights = self.weights + deltaW
-            
             
             error = self.calculateError(X)
 
@@ -37,15 +35,26 @@ class SimpleNonLinearPerceptron:
                 minW = self.weights
 
             counter += 1
-        print(minError)
+        # print(minError)
         self.weights = minW
-        # print("Weights percep:")
-        # print(self.weights)
 
     def calculateError(self, X):
         e = 0
         for j in range(len(X)):
             resultValue = X[j][1]
-            trainingValue = np.inner(self.weights, X[j][0])
+            excited = np.inner(self.weights, X[j][0])
+            trainingValue = self.activation(excited)
             e = e + (resultValue - trainingValue)** 2
         return e * 0.5
+
+    def activation(self, excited):
+        return np.tanh(excited)
+
+    def get_output(self, trainingSet):
+        print("MIN W: {}".format(self.weights))
+        outputs = []
+        X = [ [[1, y[0][0], y[0][1], y[0][2]]] for y in trainingSet]      
+        for i in range(len(X)):
+            excited_state = np.inner(X[i], self.weights)
+            outputs.append(self.activation(excited_state))
+        return outputs
--- TP3/ej2.py
+++ TP3/ej2.py
@@ -6,19 +6,19 @@ from SimpleNonLinearPerceptron import SimpleNonLinearPerceptron
 from SimpleLinearPerceptron import SimpleLinearPerceptron
 import numpy as np
 
-g, gPrime = primeFunction.getPrimeFunction(0.05,'logistic') #puede ser 'tanh' o 'logistic'
 
-trainingSet = parser.parseFile("resources/TP3-ej2-Conjuntoentrenamiento.txt")
-resultSet = parser.parseFile("resources/TP3-ej2-Salida-deseada.txt")
+##### SIMPLE LINEAL #####
+# trainingSet = parser.parseFile("resources/TP3-ej2-Conjuntoentrenamiento.txt")
+# resultSet = parser.parseFile("resources/TP3-ej2-Salida-deseada.txt")
 
-X = []
-counter = 0
-for i in trainingSet:
-    X.append([  i[0], i[1], i[2] ,resultSet[counter]])
-    counter +=1
+# X = []
+# counter = 0
+# for i in trainingSet:
+#     X.append([  i[0], i[1], i[2] ,resultSet[counter]])
+#     counter +=1
 
-slp = SimpleNonLinearPerceptron(len(trainingSet[0]), 0.01,g,gPrime)
-slp.trainPerceptron(X, 0.001,10000)
+# slp = SimpleLinearPerceptron(len(trainingSet[0]), 0.01)
+# slp.trainPerceptron(X, 0.001,10000)
 
 # counter = 0
 # for x in X:
@@ -28,3 +28,21 @@ slp.trainPerceptron(X, 0.001,10000)
 #     print('prediction: {} - esperado: {}'.format(prediction,resultSet[counter]) )
 #     counter +=1
 
+################
+
+
+##### SIMPLE NO LINEAL #####
+# Normalizo solo para el NO LINEAL
+normalized = parser.readAndNormalize("resources/TP3-ej2-Conjuntoentrenamiento.txt","resources/TP3-ej2-Salida-deseada.txt")
+
+X = []
+X = [ [[y[0], y[1], y[2]], y[3]] for y in normalized]
+
+snlp = SimpleNonLinearPerceptron(len(X[0][0]), 0.03)
+snlp.trainPerceptron(X, 0.001,10000)
+
+output = snlp.get_output(X)
+
+for x in range(len(output)):
+    print("prediction: {} -- expected: {}" .format(output[x],X[x][1] ))
+################
