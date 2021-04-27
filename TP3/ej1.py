f7bd424cba723d17ed250f0985cd8ef5a0505f3c
ahenestrosa <ahenestrosa@itba.edu.ar>
2021-04-27 00:14:51 -0300

    Results for ej1

--- TP3/ej1.py
+++ TP3/ej1.py
@@ -11,7 +11,12 @@ def andPerceptron():
     #epocas
     for i in range(100):
         perceptron.trainPerceptron(trainingSet, resultSet)
-    return perceptron
+        for i in range(len(trainingSet)):
+            r = perceptron.calculateOutput(trainingSet[i])
+            print(str(trainingSet[i][0]) + " AND " + str(trainingSet[i][1]))
+            print("Expected: " +  str(resultSet[i]))
+            print("Actual:" + str(r))
+            print()
 
 def xorPerceptron():
     perceptron = PerceptronSimpleStep(2, 0.1)
@@ -21,8 +26,14 @@ def xorPerceptron():
     #epocas
     for i in range(10):
         perceptron.trainPerceptron(trainingSet, resultSet)
-    return perceptron
+    for i in range(len(trainingSet)):
+        r = perceptron.calculateOutput(trainingSet[i])
+        print(str(trainingSet[i][0]) + " XOR " + str(trainingSet[i][1]))
+        print("Expected: " +  str(resultSet[i]))
+        print("Actual:" + str(r))
+        print()
+
 
 
 perceptron = andPerceptron()
-print(perceptron.calculateOutput([1,-1]))
+
