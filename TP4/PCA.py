5f2b1019b0c07e68ced911b421864518ce175305
francisco choi <fchoi@itba.edu.ar>
2021-05-06 10:55:38 -0300

    PCA

--- TP4/PCA.py
+++ TP4/PCA.py
@@ -16,22 +16,27 @@ countries = matrix.iloc[:, 0].values #only country names
 valuesStd = StandardScaler().fit_transform(values) #standarization of values --> z = (x - u) / s
 valuesCov = np.cov(valuesStd.T) #we need transposed matrix as numpy calculates the array as X = [x_1, x_2, ... x_N]^T
 
-autoVal, autoVec = np.linalg.eigh(valuesCov)
+autoVal, autoVec = np.linalg.eigh(valuesCov) #this is just to show what pca does inside
 
 pca = PCA().fit(valuesStd)
 
 # print(valuesCov)
 
-# print(np.cumsum(pca.explained_variance_ratio_))  #
+print(np.cumsum(pca.explained_variance_ratio_))  #
+plt.plot(np.cumsum(pca.explained_variance_ratio_))
+plt.xlim(0,7,1)
+plt.xlabel('Components')
+plt.ylabel('Variance')
+plt.show()
+
+
 #[0.46102367 0.63061273 0.78249709 0.89254794 0.95795489 0.982051161]
 
 pca_factory = PCA(n_components=5) #0.96 covered
 Y = pca_factory.fit_transform(valuesStd)
 
-print()
-
 components = pca_factory.fit(valuesStd)
-print(components.components_[0]) #this shows the influence of every variable in the first component
+# print(components.components_[0]) #this shows the influence of every variable in the first component
 #[ 0.1248739  -0.50050586  0.40651815 -0.48287333  0.18811162 -0.47570355 0.27165582]
 
 first_component = Y[:,0]
@@ -46,9 +51,12 @@ variables = ["Area", "GDP", "Inflation", "Life Expectancy", "Military", "Pop Gro
 
 #https://stackoverflow.com/questions/39216897/plot-pca-loadings-and-loading-in-biplot-in-sklearn-like-rs-autoplot
 
+# for m in range(len(first_component)):
+#         plt.scatter(first_component[m] * scalex, second_component[m] * scaley, label=countries[m])
+
 plt.scatter(first_component * scalex, second_component * scaley)
 
-print(variables)
+# print(variables)
 for i in range(n):
         plt.arrow(0, 0, coeff[i,0], coeff[i,1],color = 'r',alpha = 0.5)
         plt.text(coeff[i,0]* 1.15, coeff[i,1] * 1.15, variables[i], color = 'g', ha = 'center', va = 'center')
@@ -58,15 +66,20 @@ plt.ylim(-1,1)
 plt.xlabel("First Component")
 plt.ylabel("Second Component")
 plt.grid()
+# plt.legend()
 
 
 
+# we did not show names as points are very close
+# plt.tight_layout()
 
 # for i, txt in enumerate(countries):
-#     ax.annotate(txt, (first_component[i], second_component[i]))
-# we did not show names as points are very close
+#     plt.annotate(txt, (first_component[i], second_component[i]))
+
 
 plt.show()
+
+
 
 
 
