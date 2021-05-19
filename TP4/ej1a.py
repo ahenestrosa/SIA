0b3cabc92bd1dd46a2921d934a231c942edc2933
francisco choi <fchoi@itba.edu.ar>
2021-05-18 22:18:32 -0300

    heatmap change

--- TP4/ej1a.py
+++ TP4/ej1a.py
@@ -3,41 +3,53 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-def outputResults(location, outputFile, kohonen):
+def outputResults(outputFile, kohonen):
+    location = []
+    heatmap = np.zeros((4,4))
+    for i in range(valuesStd.shape[0]):
+        sx, sy, d = kohonen._calculateMinDistance(valuesStd[i], True)
+        aux = [sx, sy, countries[i]]
+        # xLoc.append(sx)
+        # yLoc.append(sy)
+        heatmap[sx][sy] += 1
+        location.append(aux)
     distance = kohonen.calculateNeuronsDistance()
 
     plt.clf()
     plt.imshow(distance, origin='lower')
     plt.colorbar()
-    plt.savefig('./Resources/output/' + outputFile + 'distance')
-    plt.show()
+    plt.savefig(outputFile + 'distance')
+
+
 
     sortedCountries = sorted(location, key=lambda x: (x[0], x[1]))
-    outF = open('./Resources/output/' + outputFile + '.csv', 'w')
+    outF = open(outputFile + '.csv', 'w')
     for country in sortedCountries:
         outF.write(str(country[0]) + ',' + str(country[1]) + ',' + country[2])
         outF.write('\n')
 
     outF.close()
 
-    heatmap, xedges, yedges = np.histogram2d(xLoc, yLoc, bins=netSize)
-    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+
+    # heatmap, xedges, yedges = np.histogram2d(xLoc, yLoc, bins=netSize)
+    # extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
     plt.clf()
-    plt.imshow(heatmap.T, origin='lower')
+    print(heatmap)
+    plt.imshow(heatmap)
     cb = plt.colorbar()
-    plt.savefig('./Resources/output/' + outputFile)
+    plt.savefig(outputFile)
     plt.show()
 
-
 filePath = "./Resources/europe.csv"
-oututDir = "./Output/"
+oututDir = "./Resources/output/"
 
 matrix = pd.read_csv(filePath)
 
-outputFile = oututDir + 'RFixedEtaFixedMaxArgs'
+
 
 values = matrix.iloc[:,1:8].values #only values
 countries = matrix.iloc[:, 0].values #only country names
@@ -47,27 +59,84 @@ valuesStdMat = pd.DataFrame(StandardScaler().fit_transform(values), index=countr
 
 valuesStd = valuesStdMat.values
 
-netSize = 3
-
+netSize = 4
+outputFile = oututDir + 'RFixedEtaFixed'
 kohonen = Kohonen(7, netSize)
-trainData = kohonen.trainRule(valuesStd, 1, 2, True, False, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'RFixedEtaVar'
+kohonen = Kohonen(7, netSize)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, True, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'RVarEtaVar'
+kohonen = Kohonen(7, netSize)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, True, True)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'RVarEtaFixed'
+kohonen = Kohonen(7, netSize)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, True)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'MVRFixedEtaFixed'
+kohonen = Kohonen(7, netSize, valuesStd, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'MVRFixedEtaVar'
+kohonen = Kohonen(7, netSize, valuesStd, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, True, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'MVRVarEtaVar'
+kohonen = Kohonen(7, netSize, valuesStd, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, True, True)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'MVRVarEtaFixed'
+kohonen = Kohonen(7, netSize, valuesStd, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'MVRVarEtaVarCorrelate'
+kohonen = Kohonen(7, netSize, valuesStd, False)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, False)
+
+outputResults(outputFile, kohonen)
+
+outputFile = oututDir + 'RVarEtaVarCorrelate'
+kohonen = Kohonen(7, netSize)
+trainData = kohonen.trainRule(valuesStd, 1000, 2, True, False, False)
+
+
+outputResults(outputFile, kohonen)
 
 
 
 
-location = []
-xLoc = []
-yLoc = []
-for i in range(valuesStd.shape[0]):
-    sx, sy, d= kohonen._calculateMinDistance(valuesStd[i], True)
-    aux = [sx,sy,countries[i]]
-    xLoc.append(sx)
-    yLoc.append(sy)
-    location.append(aux)
 
 
 
-fig, ax = plt.subplots()
 
-ax.scatter(range(len(trainData)), trainData)
+
+
+
+
+
+
+# fig, ax = plt.subplots()
+#
+# ax.scatter(range(len(trainData)), trainData)
+#
+# plt.show()
 
