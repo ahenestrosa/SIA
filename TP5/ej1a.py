a47b12596f0bf3ae6dae5013d8261b827c5fd183
francisco choi <fchoi@itba.edu.ar>
2021-06-14 22:58:44 -0300

    two dimensions rep

--- TP5/ej1a.py
+++ TP5/ej1a.py
@@ -5,22 +5,35 @@ import fonts as ft
 from copy import deepcopy
 import random as rd
 from PerceptronMultilayer import PerceptronMultilayer
+from utils import printMatrix, arrayTo2DMatrix
 
 
 #sets the sample taken to train the multilayer
-samplePer = 6/32
+samplePer = 1
 
+middlelayer = [2]
 
-
-
+activationSearch = (len(middlelayer) + 2) // 2
 
 font1sample = (rd.sample(ft.font1bitmap, int(len(ft.font1bitmap)*samplePer))) #formats the input and takes a random sample from the fonts
 
 
 
-multiLayerPerceptron = PerceptronMultilayer(35,35,[8,4,2], 'tanh', 0.01)
-multiLayerPerceptron.train(0.001, 2000, font1sample, font1sample, verbose=True)
+multiLayerPerceptron = PerceptronMultilayer(35,35,middlelayer, 'tanh', 0.01,costFunction='entropic', momentum=0.9, adaptative=(0.0001, 0.00001))
+multiLayerPerceptron.train(10, 500, font1sample, font1sample, verbose=True)
 
+
+for letter in font1sample:
+    print('############################################################')
+    r, a, v = multiLayerPerceptron.calculateOutput(letter)
+    print("Expected: " )
+    printMatrix(arrayTo2DMatrix(letter,5))
+    print("Actual:")
+    printMatrix(arrayTo2DMatrix(r,5))
+
+    print(a[activationSearch])
+    # print("Classified: " +  str(classify(r[0,0])))
+    # print()
 
 
 
---
+++ TP5/utils.py
@@ -0,0 +1,23 @@
+def printMatrix(mat):
+    for i in range(len(mat)):
+        line = ''
+        for j in range(len(mat[1])):
+            aux = '1' if mat[i][j] >= 0 else ' '
+            line += aux + " "
+        print(line)
+
+def arrayTo2DMatrix(arr, shape0): #shape 1 is determined by len(arr)/shape0
+    counter = 0
+    matrix = []
+    aux = []
+    for i in arr:
+        if counter == shape0:
+            matrix.append(aux)
+            aux = []
+            counter = 0
+        aux.append(i)
+
+        counter += 1
+    matrix.append(aux)
+
+    return matrix
