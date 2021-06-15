3aabd0b64f56e57aa18342730cb43b2e54cb892b
francisco choi <fchoi@itba.edu.ar>
2021-06-15 12:39:45 -0300

    generates new letters

--- TP5/ej1a.py
+++ TP5/ej1a.py
@@ -5,35 +5,45 @@ import fonts as ft
 from copy import deepcopy
 import random as rd
 from PerceptronMultilayer import PerceptronMultilayer
-from utils import printMatrix, arrayTo2DMatrix
+from utils import printMatrix, arrayTo2DMatrix, outputFromLayerReshape
 
 
 #sets the sample taken to train the multilayer
-samplePer = 1
+samplePer = 0.125
 
 middlelayer = [2]
 
-activationSearch = (len(middlelayer) + 2) // 2
+activationSearch = (len(middlelayer) + 1) // 2
 
 font1sample = (rd.sample(ft.font1bitmap, int(len(ft.font1bitmap)*samplePer))) #formats the input and takes a random sample from the fonts
 
 
-
 multiLayerPerceptron = PerceptronMultilayer(35,35,middlelayer, 'tanh', 0.01,costFunction='entropic', momentum=0.9, adaptative=(0.0001, 0.00001))
+
 multiLayerPerceptron.train(10, 500, font1sample, font1sample, verbose=True)
 
 
-for letter in font1sample:
-    print('############################################################')
-    r, a, v = multiLayerPerceptron.calculateOutput(letter)
-    print("Expected: " )
-    printMatrix(arrayTo2DMatrix(letter,5))
-    print("Actual:")
-    printMatrix(arrayTo2DMatrix(r,5))
+# for letter in font1sample:
+#     print('############################################################')
+#     r, a, v = multiLayerPerceptron.calculateOutput(letter)
+#     print("Expected: " )
+#     printMatrix(arrayTo2DMatrix(letter,5))
+#     print("Actual:")
+#     printMatrix(arrayTo2DMatrix(r,5))
+#
+#     print(a[activationSearch])
+#     # print("Classified: " +  str(classify(r[0,0])))
+#     # print()
 
-    print(a[activationSearch])
-    # print("Classified: " +  str(classify(r[0,0])))
-    # print()
+
+print("Generation of Letter")
+
+for i in range(3):
+    oLay = [rd.random() * 2 - 1 for i in range(2)]
+    nLetter = multiLayerPerceptron.forwardPropagateFromLayer(oLay, activationSearch)
+    print(len(nLetter))
+    printMatrix(outputFromLayerReshape(nLetter, 5))
+
 
 
 
