301f8a5279f6794d5237588fe1776d5e7d619f42
Nicolas de la Torre <ndelatorre@itba.edu.ar>
2021-06-15 11:13:36 -0300

    update but not working

--- TP5/utils.py
+++ TP5/utils.py
@@ -13,3 +13,27 @@ def mapToBlack(digit):
             bitmap[i][j] = r
 
     return bitmap
+
+def printMatrix(mat):
+    for i in range(len(mat)):
+        line = ''
+        for j in range(len(mat[1])):
+            aux = '*' if mat[i][j] >= 0 else ' '
+            line += aux + " "
+        print(line)
+
+def arrayTo2DMatrix(arr, shape0): #shape 1 is determined by len(arr)/shape0
+    counter = 0
+    matrix = []
+    aux = []
+    for i in arr:
+        if counter == shape0:
+            matrix.append(aux)
+            aux = []
+            counter = 0
+        aux.append(i)
+
+        counter += 1
+    matrix.append(aux)
+
+    return matrix
