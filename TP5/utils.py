d1d38b42d8750ccdfc914c6f750ec06974892c8d
ahenestrosa <ahenestrosa@itba.edu.ar>
2021-06-15 01:05:51 -0300

    Vae for fonts

---
+++ TP5/utils.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+
+def mapToBlack(digit):
+    bitmap = np.zeros((7,5))
+    i = 0
+    for i in range(len(digit)):
+        for j in range(len(digit[i])):
+            elem = digit[i][j]
+            r = 1
+            if elem >= 0:
+                r = 0
+            bitmap[i][j] = r
+
+    return bitmap
