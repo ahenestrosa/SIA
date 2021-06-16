39964f15871a46665773c206904be0b8840f56eb
ahenestrosa <ahenestrosa@itba.edu.ar>
2021-06-16 14:00:08 -0300

    Showing images for dae

--- TP5/ej1b.py
+++ TP5/ej1b.py
@@ -3,6 +3,8 @@ import fonts as ft
 import random as rd
 from PerceptronMultilayer import PerceptronMultilayer
 import utils 
+import matplotlib.pyplot as plt
+
 
 def classify(num):
     if num > 0:
@@ -28,17 +30,46 @@ def printSymbol(symbol):
         print()
 
 
+def showColorMap(original, modified, output, filename):
+    n = 7
+    digit_width = 5
+    digit_height = 7
+    figure = np.ones((digit_height * n, digit_width * 3))
+
+    for i, yi in enumerate(range(n)):
+        figure[i * digit_height: (i + 1) * digit_height,
+            0 * digit_width: (0 + 1) * digit_width] = 1- original[i].reshape(digit_height, digit_width)
+        figure[i * digit_height: (i + 1) * digit_height,
+            1 * digit_width: (1 + 1) * digit_width] = 1-modified[i].reshape(digit_height, digit_width)
+        figure[i * digit_height: (i + 1) * digit_height,
+            2 * digit_width: (2 + 1) * digit_width] = 1-output[i].reshape(digit_height, digit_width)
+        
+
+    plt.figure(figsize=(10, 10))
+    plt.imshow(figure , cmap='Greys_r')
+
+    xline = np.linspace(-0.5, digit_width*3 - 0.5)
+    for i in range(1, n):
+        plt.plot(xline, np.array([-0.5+digit_height*i for t in range(len(xline))]), color='red', linewidth=3)
+
+    yline = np.linspace(-0.5, digit_height*n - 0.5)
+    for i in range(1, 3):
+        plt.plot(np.array([-0.5+digit_width*i for t in range(len(yline))]) ,yline, color='red', linewidth=3)
+
+
+    # plt.show()
+    plt.savefig(filename + '.png', bbox_inches='tight')
 
 
 
 def multipleSamplePercentaje(fontBitmap,pm):
-    sampleP = [0.25, 0.5 , 0.75]
+    sampleP = [0.25, 0.5 , 0.75, 1]
     fontModified = []
     for i in sampleP:
         fontModified.clear()
         print(f'{i} of dataset')
-        font = (rd.sample(fontBitmap, int(len(fontBitmap)*i)))
-        # for k in range(len(font)):
+        font = fontBitmap[0:int(len(fontBitmap)*i)]
+                # for k in range(len(font)):
         #     print("\n~~~ Original ~~~\n")
         #     printSymbol(font[k].reshape(7,5))
         for p in range(len(font)):
@@ -47,6 +78,7 @@ def multipleSamplePercentaje(fontBitmap,pm):
             # printSymbol(fontModified[p].reshape(7,5))
         multiLayerPerceptron = PerceptronMultilayer(35,35,[12], 'tanh', 0.01,costFunction='entropic', momentum=0.9, adaptative=(0.0001, 0.00001))
         multiLayerPerceptron.train(10, 500, fontModified, font)
+        outputs = []
         for i in range(len(fontModified)):
             print('############################################################')
             r, a, v = multiLayerPerceptron.calculateOutput(font[i])
@@ -56,16 +88,17 @@ def multipleSamplePercentaje(fontBitmap,pm):
             utils.printMatrix(utils.arrayTo2DMatrix(fontModified[i],5))
             print("Autoencoder Output: ")
             utils.printMatrix(utils.arrayTo2DMatrix(r,5))
-
+            outputs.append(r)
+        showColorMap(font, fontModified, outputs, "Dae_" + str(pm) + "_" + str(i))
 
 # 10% noise
-multipleSamplePercentaje(ft.font1bitmap,0.10)
+multipleSamplePercentaje(ft.font2bitmap,0.10)
 
 # 20% noise 
-multipleSamplePercentaje(ft.font1bitmap,0.20)
+multipleSamplePercentaje(ft.font2bitmap,0.20)
 
 # 30% noise 
-multipleSamplePercentaje(ft.font1bitmap,0.30)
+multipleSamplePercentaje(ft.font2bitmap,0.30)
 """
 
 font = (rd.sample(ft.font1bitmap, int(len(ft.font1bitmap)*0.5)))
